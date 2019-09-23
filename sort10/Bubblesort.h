1a98314077af2f0d4c89fffbb25661265f14cb48
FrezCirno <2277861660@qq.com>
2019-09-23 18:23:18 +0800

    sort1

--- sort10/Bubblesort.h
+++ sort10/Bubblesort.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Common.h"
+void Bubblesort(int *a, int n, comparer compare, swaper swap) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int k = i; k < n - 1; k++) {
+            if (compare(a[k], a[k + 1]) > 0) {
+                swap(a[k], a[k + 1]);
+            }
+        }
+    }
+}
--- sort10/Selectsort.h
+++ sort10/Selectsort.h
@@ -1,1 +1,5 @@
 #pragma once
+#include "Common.h"
+void Selectsort() {
+    
+}
