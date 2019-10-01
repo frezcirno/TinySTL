7ae66f2b419c7742dcf6934595b20c95f4604acb
FrezCirno <2277861660@qq.com>
2019-10-01 18:45:58 +0800

    CommonDS deque vector

---
+++ commonDS/Deque.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <deque>
+template <typename T, int >
+class Deque
+{
+public:
+    Deque() {}
+    ~Deque() {}
+
+
+    private:
+
+};
+int main(int argc, char const *argv[])
+{
+    std::deque<int> d;
+    return 0;
+}
---
+++ commonDS/Stack.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stack>
+template <typename T>
+class Stack {
+   public:
+    Stack();
+    ~Stack();
+    void push_back(const T& elem);
+    void pop_back();
+   private:
+};
+int main(int argc, char const *argv[])
+{
+    std::stack<int>s;
+    stack
+    return 0;
+}
+
